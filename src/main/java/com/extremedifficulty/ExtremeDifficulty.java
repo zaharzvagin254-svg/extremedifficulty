ff7ff4217988060043a8e365e2f158e702dfd2dd
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-13 13:50:36 +0300

    Update ExtremeDifficulty.java

--- src/main/java/com/extremedifficulty/ExtremeDifficulty.java
+++ src/main/java/com/extremedifficulty/ExtremeDifficulty.java
@@ -5,9 +5,26 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.TickEvent;
-import net.minecraft.world.entity.monster.*;
-import net.minecraft.world.entity.monster.hoglin.Hoglin;
-import net.minecraft.world.entity.monster.hoglin.Zoglin;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.entity.monster.Husk;
+import net.minecraft.world.entity.monster.Stray;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Spider;
+import net.minecraft.world.entity.monster.CaveSpider;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Drowned;
+import net.minecraft.world.entity.monster.Witch;
+import net.minecraft.world.entity.monster.Vindicator;
+import net.minecraft.world.entity.monster.Pillager;
+import net.minecraft.world.entity.monster.Evoker;
+import net.minecraft.world.entity.monster.Ravager;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
+import net.minecraft.world.entity.monster.Guardian;
+import net.minecraft.world.entity.monster.ElderGuardian;
+import net.minecraft.world.entity.monster.Blaze;
+import net.minecraft.world.entity.monster.Ghast;
+import net.minecraft.world.entity.monster.WitherSkeleton;
 import net.minecraft.world.entity.monster.piglin.Piglin;
 import net.minecraft.world.entity.monster.piglin.PiglinBrute;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
@@ -30,122 +47,76 @@ public class ExtremeDifficulty {
         LOGGER.info("[ExtremeDifficulty] Mod loaded! Mobs are now stronger.");
     }
 
-    // Called when any entity spawns into the world
     @SubscribeEvent
     public void onEntityJoin(EntityJoinLevelEvent event) {
         Entity entity = event.getEntity();
         Level level = event.getLevel();
-
         if (!(entity instanceof LivingEntity living)) return;
         if (level.isClientSide()) return;
-
-        // Determine if it's night or nether/end (always night stats)
-        boolean isNight = isNightOrDangerousDimension(level);
-
-        applyBuffs(living, isNight);
+        applyBuffs(living, isNight(level));
     }
 
-    // Called every server tick - reapply buffs in case Forge reset them
     @SubscribeEvent
     public void onServerTick(TickEvent.LevelTickEvent event) {
         if (event.phase != TickEvent.Phase.END) return;
         if (!(event.level instanceof ServerLevel serverLevel)) return;
-
-        // Only run every 40 ticks (2 seconds) to save performance
         if (serverLevel.getGameTime() % 40 != 0) return;
-
-        boolean isNight = isNightOrDangerousDimension(serverLevel);
-
-        // Apply to all living entities in loaded chunks
-        serverLevel.getAllEntities().forEach(entity -> {
-            if (entity instanceof LivingEntity living) {
-                applyBuffs(living, isNight);
-            }
+        boolean night = isNight(serverLevel);
+        serverLevel.getAllEntities().forEach(e -> {
+            if (e instanceof LivingEntity living) applyBuffs(living, night);
         });
     }
 
-    private boolean isNightOrDangerousDimension(Level level) {
-        // Nether and End are always "night mode"
+    private boolean isNight(Level level) {
         if (level.dimension() == Level.NETHER) return true;
         if (level.dimension() == Level.END) return true;
-        // Overworld: check time (13000-23000 = night)
         long time = level.getDayTime() % 24000;
         return time >= 13000 && time <= 23000;
     }
 
-    private void applyBuffs(LivingEntity entity, boolean isNight) {
-        if (entity instanceof Zombie) {
-            setStats(entity, isNight ? 50 : 35, isNight ? 7 : 5, isNight ? 0.40 : 0.32);
-        } else if (entity instanceof Skeleton) {
-            setStats(entity, isNight ? 45 : 32, isNight ? 6 : 5, isNight ? 0.36 : 0.28);
-        } else if (entity instanceof Husk) {
-            setStats(entity, isNight ? 50 : 35, isNight ? 8 : 6, isNight ? 0.38 : 0.30);
-        } else if (entity instanceof Stray) {
-            setStats(entity, isNight ? 45 : 32, isNight ? 8 : 6, isNight ? 0.36 : 0.28);
-        } else if (entity instanceof Creeper) {
-            setStats(entity, isNight ? 42 : 30, isNight ? 4 : 3, isNight ? 0.38 : 0.30);
-        } else if (entity instanceof Spider) {
-            setStats(entity, isNight ? 36 : 26, isNight ? 5 : 4, isNight ? 0.52 : 0.42);
-        } else if (entity instanceof CaveSpider) {
-            setStats(entity, isNight ? 28 : 20, isNight ? 5 : 4, isNight ? 0.56 : 0.45);
-        } else if (entity instanceof EnderMan) {
-            setStats(entity, isNight ? 90 : 65, isNight ? 14 : 11, isNight ? 0.44 : 0.35);
-        } else if (entity instanceof Drowned) {
-            setStats(entity, isNight ? 48 : 34, isNight ? 8 : 6, isNight ? 0.36 : 0.28);
-        } else if (entity instanceof Witch) {
-            setStats(entity, isNight ? 58 : 42, isNight ? 4 : 3, isNight ? 0.36 : 0.28);
-        } else if (entity instanceof Vindicator) {
-            setStats(entity, isNight ? 56 : 40, isNight ? 20 : 15, isNight ? 0.40 : 0.32);
-        } else if (entity instanceof Pillager) {
-            setStats(entity, isNight ? 52 : 38, isNight ? 8 : 6, isNight ? 0.38 : 0.30);
-        } else if (entity instanceof Evoker) {
-            setStats(entity, isNight ? 52 : 38, isNight ? 6 : 4, isNight ? 0.36 : 0.28);
-        } else if (entity instanceof Ravager) {
-            setStats(entity, isNight ? 240 : 175, isNight ? 26 : 20, isNight ? 0.38 : 0.30);
-        } else if (entity instanceof ZombifiedPiglin) {
-            setStats(entity, isNight ? 48 : 35, isNight ? 13 : 10, isNight ? 0.38 : 0.30);
-        } else if (entity instanceof PiglinBrute) {
-            setStats(entity, isNight ? 115 : 85, isNight ? 36 : 28, isNight ? 0.46 : 0.38);
-        } else if (entity instanceof Piglin) {
-            setStats(entity, isNight ? 38 : 28, isNight ? 11 : 8, isNight ? 0.40 : 0.32);
-        } else if (entity instanceof Hoglin) {
-            setStats(entity, isNight ? 85 : 65, isNight ? 14 : 10, isNight ? 0.40 : 0.32);
-        } else if (entity instanceof Zoglin) {
-            setStats(entity, isNight ? 85 : 65, isNight ? 14 : 10, isNight ? 0.40 : 0.32);
-        } else if (entity instanceof Guardian) {
-            setStats(entity, isNight ? 68 : 50, isNight ? 14 : 10, isNight ? 0.30 : 0.25);
-        } else if (entity instanceof ElderGuardian) {
-            setStats(entity, isNight ? 175 : 130, isNight ? 22 : 16, isNight ? 0.30 : 0.25);
-        } else if (entity instanceof Blaze) {
-            setStats(entity, isNight ? 46 : 34, isNight ? 11 : 8, isNight ? 0.30 : 0.25);
-        } else if (entity instanceof Ghast) {
-            setStats(entity, isNight ? 18 : 14, isNight ? 8 : 6, isNight ? 0.30 : 0.25);
-        } else if (entity instanceof WitherSkeleton) {
-            setStats(entity, isNight ? 80 : 60, isNight ? 16 : 12, isNight ? 0.36 : 0.30);
-        } else if (entity instanceof WitherBoss) {
-            setStats(entity, isNight ? 650 : 500, isNight ? 20 : 15, isNight ? 0.30 : 0.25);
-        } else if (entity instanceof EnderDragon) {
-            setMaxHealth(entity, isNight ? 420 : 320);
-        }
+    private void applyBuffs(LivingEntity e, boolean n) {
+        if (e instanceof Zombie)               setStats(e, n?50:35,   n?7:5,   n?0.40:0.32);
+        else if (e instanceof Skeleton)        setStats(e, n?45:32,   n?6:5,   n?0.36:0.28);
+        else if (e instanceof Husk)            setStats(e, n?50:35,   n?8:6,   n?0.38:0.30);
+        else if (e instanceof Stray)           setStats(e, n?45:32,   n?8:6,   n?0.36:0.28);
+        else if (e instanceof Creeper)         setStats(e, n?42:30,   n?4:3,   n?0.38:0.30);
+        else if (e instanceof CaveSpider)      setStats(e, n?28:20,   n?5:4,   n?0.56:0.45);
+        else if (e instanceof Spider)          setStats(e, n?36:26,   n?5:4,   n?0.52:0.42);
+        else if (e instanceof EnderMan)        setStats(e, n?90:65,   n?14:11, n?0.44:0.35);
+        else if (e instanceof Drowned)         setStats(e, n?48:34,   n?8:6,   n?0.36:0.28);
+        else if (e instanceof Witch)           setStats(e, n?58:42,   n?4:3,   n?0.36:0.28);
+        else if (e instanceof Vindicator)      setStats(e, n?56:40,   n?20:15, n?0.40:0.32);
+        else if (e instanceof Pillager)        setStats(e, n?52:38,   n?8:6,   n?0.38:0.30);
+        else if (e instanceof Evoker)          setStats(e, n?52:38,   n?6:4,   n?0.36:0.28);
+        else if (e instanceof Ravager)         setStats(e, n?240:175, n?26:20, n?0.38:0.30);
+        else if (e instanceof ZombifiedPiglin) setStats(e, n?48:35,   n?13:10, n?0.38:0.30);
+        else if (e instanceof PiglinBrute)     setStats(e, n?115:85,  n?36:28, n?0.46:0.38);
+        else if (e instanceof Piglin)          setStats(e, n?38:28,   n?11:8,  n?0.40:0.32);
+        else if (e instanceof ElderGuardian)   setStats(e, n?175:130, n?22:16, n?0.30:0.25);
+        else if (e instanceof Guardian)        setStats(e, n?68:50,   n?14:10, n?0.30:0.25);
+        else if (e instanceof Blaze)           setStats(e, n?46:34,   n?11:8,  n?0.30:0.25);
+        else if (e instanceof Ghast)           setStats(e, n?18:14,   n?8:6,   n?0.30:0.25);
+        else if (e instanceof WitherSkeleton)  setStats(e, n?80:60,   n?16:12, n?0.36:0.30);
+        else if (e instanceof WitherBoss)      setStats(e, n?650:500, n?20:15, n?0.30:0.25);
+        else if (e instanceof EnderDragon)     setMaxHp(e, n?420:320);
     }
 
-    private void setStats(LivingEntity entity, double hp, double damage, double speed) {
-        setMaxHealth(entity, hp);
-        if (entity.getAttribute(Attributes.ATTACK_DAMAGE) != null)
-            entity.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(damage);
-        if (entity.getAttribute(Attributes.MOVEMENT_SPEED) != null)
-            entity.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(speed);
+    private void setStats(LivingEntity e, double hp, double dmg, double spd) {
+        setMaxHp(e, hp);
+        if (e.getAttribute(Attributes.ATTACK_DAMAGE) != null)
+            e.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(dmg);
+        if (e.getAttribute(Attributes.MOVEMENT_SPEED) != null)
+            e.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(spd);
     }
 
-    private void setMaxHealth(LivingEntity entity, double hp) {
-        if (entity.getAttribute(Attributes.MAX_HEALTH) != null) {
-            double currentHp = entity.getHealth();
-            double currentMax = entity.getMaxHealth();
-            entity.getAttribute(Attributes.MAX_HEALTH).setBaseValue(hp);
-            // Scale current HP proportionally
-            if (currentMax > 0) {
-                float newHp = (float) (currentHp / currentMax * hp);
-                entity.setHealth(Math.max(newHp, (float)currentHp));
+    private void setMaxHp(LivingEntity e, double hp) {
+        if (e.getAttribute(Attributes.MAX_HEALTH) != null) {
+            float cur = e.getHealth();
+            float curMax = e.getMaxHealth();
+            e.getAttribute(Attributes.MAX_HEALTH).setBaseValue(hp);
+            if (curMax > 0) {
+                float newHp = (float)(cur / curMax * hp);
+                e.setHealth(Math.max(newHp, cur));
             }
         }
     }
