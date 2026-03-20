672cd1a611931a1fc4bf04615f940a6acffba869
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-20 21:53:13 +0300

    Update ExtremeDifficulty.java

--- src/main/java/com/extremedifficulty/ExtremeDifficulty.java
+++ src/main/java/com/extremedifficulty/ExtremeDifficulty.java
@@ -63,6 +63,7 @@ public class ExtremeDifficulty {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new SpawnHandler());
         MinecraftForge.EVENT_BUS.register(new MobAIHandler());
+        MinecraftForge.EVENT_BUS.register(new SoundAlertHandler());
         LOGGER.info("[ExtremeDifficulty] Mod loaded!");
     }
 
