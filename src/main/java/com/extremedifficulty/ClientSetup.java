eada8f76b953babb940b9ea9276da42f550a879d
zaharzvagin254-svg <zaharzvagin254@gmail.com>
2026-03-20 15:29:33 +0300

    Update ClientSetup.java

--- src/main/java/com/extremedifficulty/ClientSetup.java
+++ src/main/java/com/extremedifficulty/ClientSetup.java
@@ -4,17 +4,12 @@ import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.MinecraftForge;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientSetup {
 
     public static void registerRenderers() {
-        // Кастомный рендерер зомби (светящиеся глаза)
         EntityRenderers.register(EntityType.ZOMBIE, GlowingZombieRenderer::new);
         EntityRenderers.register(EntityType.HUSK,   GlowingZombieRenderer::new);
-
-        // Красный экранный фильтр судной ночи
-        MinecraftForge.EVENT_BUS.register(new BloodMoonScreenEffect());
     }
 }
